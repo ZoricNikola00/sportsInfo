771c62436348a03bf972f057752b36e3ea429823
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-02 00:34:23 +0100

    Adding Search and minor edit

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import { Link } from 'react-router-dom'
 import { FaSearch, FaBars } from 'react-icons/fa'
+import Search from './Search'
 
 
 const SearchBtn=()=>{
@@ -21,7 +22,7 @@ const Navbar = () => {
   return (
     <nav className="w-[95%] md:w-[80%] text-blue-500 font-bold italic flex items-center justify-between p-4 rounded mx-auto my-4 bg-slate-100 cShadow">
         <div data-attribute='modal' className={`flex justify-center items-center fixed top-0 left-0 w-full h-full text-black bg-blue-600/90 transition-all transition-500 ${searchModal?'opacity-100 z-30':'opacity-0 -z-30'}`}>
-
+            <Search/>
         </div>
         <div>
             <Link to='/'><h1 className='text-4xl'>SportsInfo</h1></Link>
@@ -38,7 +39,7 @@ const Navbar = () => {
             :
         <div className='hidden md:flex items-center'>
             <FaSearch className='searchBtn' onClick={_=>setSearchModal(true)} />
-            <Link to='signIn' className='w-fit cBtn'>Sign In</Link>
+            <Link to='signIn' onClick={_=>{setSearchModal(false);setSidebar(false)}} className='w-fit cBtn'>Sign In</Link>
         </div>
         }
         <div className='text-4xl cursor-pointer flex items-center md:hidden'>
@@ -57,7 +58,7 @@ const Navbar = () => {
             </div>
                 :
             <div className='flex items-center flex-col h-full'>
-                <Link to='signIn' className='w-fit cBtn'>Sign In</Link>
+                <Link to='signIn' onClick={_=>{setSearchModal(false);setSidebar(false)}} className='w-fit cBtn'>Sign In</Link>
             </div>
             }
             </div>
