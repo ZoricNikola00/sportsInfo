3b60aa89e62f5a914cecb1b71ae5db659217f03e
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-02 00:34:41 +0100

    Creating routes for Home and Auth page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import Navbar from "./components/Navbar"
+import {Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home"
+import Auth from "./pages/Auth"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path="/signIn" element={<Auth/>}/>
+        </Routes>
+    </>
   )
 }
 
