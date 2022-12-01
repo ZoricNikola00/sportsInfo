cf382a80e8dbf03b3c52fe0bcf9097d3afce5a72
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-01 22:54:56 +0100

    Setup startup server with express and mongoose

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import postRoutes from './routes/postRoutes.js'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const app=express()
+
+app.use(bodyParser.json({limit:'30mb'}))
+app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
+app.use(cors())
+
+app.use('/', postRoutes)
+
+const mongoURI=process.env.MONGO_DB_CODE || '';
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(mongoURI)
+.then(()=>{app.listen(PORT), console.log(`Server is running on port ${PORT}`)})
+.catch(err=>console.log(err))
