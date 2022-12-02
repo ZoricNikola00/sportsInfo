f995908121987acf1e6bec0e32bfb37a7a677eca
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-02 13:28:33 +0100

    Adding form component switch btn

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -9,14 +9,18 @@ function useQuery() {
 const Home = () => {
   const query=useQuery()
   const page=query.get('page') || 1;
-  const {getPosts,state}=useGlobalContext()
+  const {getPosts,state,setFormComponent,formComponent}=useGlobalContext()
   useEffect(()=>{
     getPosts(page)
     console.log(state);
     
   },[])
   return (
-    <div>Home</div>
+    <div className='w-[90%] md:w-[70%] flex flex-col mx-auto my-4'>
+      <button onClick={_=>setFormComponent(formComponent?false:true)} className='w-[200px] md:w-[300px] mx-auto cBtn'>
+        {formComponent?'Back to the posts':'Create a post'}
+      </button>
+    </div>
   )
 }
 
