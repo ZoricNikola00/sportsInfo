c267f2bf043f75f28a23287bc983f5cfcd4b1fa9
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-02 15:56:12 +0100

    Add post component and call getPosts req

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useLocation } from 'react-router-dom'
 import {useEffect} from 'react'
 import { useGlobalContext } from '../context';
 import Form from '../components/Form';
+import Posts from '../components/Posts';
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
@@ -11,7 +12,12 @@ const Home = () => {
   const query=useQuery()
   const page=query.get('page') || 1;
   const {getPosts,state,setFormComponent,formComponent}=useGlobalContext()
- 
+ useEffect(()=>{
+  getPosts(page)
+ },[page]
+ )
+ console.log(state);
+
   return (
     <div className='w-[90%] md:w-[70%] flex flex-col mx-auto my-4'>
       <button onClick={_=>setFormComponent(formComponent?false:true)} className='w-[200px] md:w-[300px] mx-auto cBtn'>
@@ -19,8 +25,9 @@ const Home = () => {
       </button>
       {formComponent?
       <Form/>:
-      <div></div>
-
+      <>
+        <Posts/>
+      </>
       }
     </div>
   )
