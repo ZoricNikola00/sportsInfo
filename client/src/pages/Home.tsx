c5a063e86a879a06493c6890fe472e9c3d4068cb
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-02 13:18:56 +0100

    Creating page number recognition and add getpost

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import { useLocation } from 'react-router-dom'
+import {useEffect} from 'react'
+import { useGlobalContext } from '../context';
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 
 const Home = () => {
+  const query=useQuery()
+  const page=query.get('page') || 1;
+  const {getPosts,state}=useGlobalContext()
+  useEffect(()=>{
+    getPosts(page)
+    console.log(state);
+    
+  },[])
   return (
     <div>Home</div>
   )
