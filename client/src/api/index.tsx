ae354e4ec4ac61f35186b5b9278bccf2842b031d
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-02 15:29:21 +0100

    Create axios request for add post

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
+import { postDataType } from "../context";
 
 const API=axios.create({baseURL:'http://localhost:5000/'})
 
 
 export const getPosts=(currentPage:number|string)=>API.get(`/posts?page=${currentPage}`)
+export const addPost=(data:postDataType)=>API.post('/posts',data)
