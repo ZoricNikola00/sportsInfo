934750d7ff72529407c83efb67f00a026ec05267
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-02 15:12:29 +0100

    Add form component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from 'react-router-dom'
 import {useEffect} from 'react'
 import { useGlobalContext } from '../context';
+import Form from '../components/Form';
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
@@ -10,16 +11,17 @@ const Home = () => {
   const query=useQuery()
   const page=query.get('page') || 1;
   const {getPosts,state,setFormComponent,formComponent}=useGlobalContext()
-  useEffect(()=>{
-    getPosts(page)
-    console.log(state);
-    
-  },[])
+ 
   return (
     <div className='w-[90%] md:w-[70%] flex flex-col mx-auto my-4'>
       <button onClick={_=>setFormComponent(formComponent?false:true)} className='w-[200px] md:w-[300px] mx-auto cBtn'>
         {formComponent?'Back to the posts':'Create a post'}
       </button>
+      {formComponent?
+      <Form/>:
+      <div></div>
+
+      }
     </div>
   )
 }
