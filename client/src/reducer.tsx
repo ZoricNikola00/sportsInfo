1ba16888cec6de5f67fd924e7bc092c7ef3975b2
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-02 15:32:13 +0100

    Adding start and end loading

--- client/src/reducer.tsx
+++ client/src/reducer.tsx
@@ -3,5 +3,10 @@ export const reducer = (state:any,action:any)=>{
     switch(action.type){
         case 'GET_POSTS':
             return {...state}
+        case "START_LOADING":
+            return {...state, loading:true}
+        case "END_LOADING":
+            return {...state, loading:false}
+        
     }
 }
