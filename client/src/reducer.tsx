ba1ffcf3362221e43c68ae344c7e76e32b2b5e57
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-02 15:41:00 +0100

    Fixig get post and adding add post func

--- client/src/reducer.tsx
+++ client/src/reducer.tsx
@@ -2,11 +2,12 @@
 export const reducer = (state:any,action:any)=>{
     switch(action.type){
         case 'GET_POSTS':
-            return {...state}
+            return {...state, posts:action.payload.data, numberOfPages:action.payload.numberOfPages, currentPage:action.payload.page}
         case "START_LOADING":
             return {...state, loading:true}
         case "END_LOADING":
             return {...state, loading:false}
-        
+        case "ADD_POST":
+            return {...state, posts:[...state.posts,action.payload]}
     }
 }
