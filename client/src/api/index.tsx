91eec005bdef9cf88d0aff17329b0f7d1e2ecb38
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 00:57:45 +0100

    Create axios request for authorization

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,8 +1,11 @@
 import axios from "axios";
-import { postDataType } from "../context";
+import { postDataType, userData } from "../context";
 
 const API=axios.create({baseURL:'http://localhost:5000/'})
 
 
 export const getPosts=(currentPage:number|string)=>API.get(`/posts?page=${currentPage}`)
 export const addPost=(data:postDataType)=>API.post('/posts',data)
+
+export const signIn=(data:userData)=>API.post('/user/signin', data)
+export const signUp=(data:userData)=>API.post('/user/signup', data)
