8fdae1c48f2c8b52913a629f7688dd4ec9492385
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 18:02:42 +0100

    Create axios request for search

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { postDataType, userData } from "../context";
+import { postDataType, SearchData, userData } from "../context";
 
 const API=axios.create({baseURL:'http://localhost:5000/'})
 
@@ -12,6 +12,7 @@ API.interceptors.request.use((req:any)=>{
 
 export const getPosts=(currentPage:number|string)=>API.get(`/posts?page=${currentPage}`)
 export const addPost=(data:postDataType)=>API.post('/posts',data)
+export const searchPosts=(searchData:SearchData)=>API.get(`/posts/search?searchQuery=${searchData.term || 'none'}&tags=${searchData.tags}`)
 
 export const signIn=(data:userData)=>API.post('/user/signin', data)
 export const signUp=(data:userData)=>API.post('/user/signup', data)
