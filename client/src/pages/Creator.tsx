894158051dd0a0dfa917789d4dce5246ddad42f7
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 18:30:14 +0100

    Add loader

--- client/src/pages/Creator.tsx
+++ client/src/pages/Creator.tsx
@@ -2,6 +2,7 @@ import {useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 import Post from '../components/Post';
 import { postDataType, useGlobalContext } from '../context';
+import { DotLoader } from 'react-spinners'
 
 const Creator = () => {
     const {name}=useParams()
@@ -10,6 +11,9 @@ const Creator = () => {
     useEffect(()=>{
         getPostsOfCreator(name)
     },[])
+    if(state.loading){
+        return <DotLoader color='rgb(59 130 246)' className=' w-full mx-auto my-24'>LOADING!!!!</DotLoader>
+       }
   return (
     <div className='w-[90%] md:w-[70%] flex flex-wrap items-center justify-center mx-auto my-4'>
         {state.posts?.map((post:postDataType)=><Post key={post._id} {...post}/>)}
