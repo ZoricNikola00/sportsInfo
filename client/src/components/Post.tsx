a15e3b77fe422129007a7f25a0cf62891122bb65
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 22:22:39 +0100

    Add like post func

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { postDataType } from '../context'
+import { postDataType, useGlobalContext } from '../context'
 import { Link } from 'react-router-dom'
 import { FaEllipsisH, FaRegThumbsUp, FaThumbsUp } from 'react-icons/fa'
 import moment from 'moment'
 
 const Post = ({_id,name,createdAt,file,info,likes,tags,title}:postDataType) => {
+    const {likePost}=useGlobalContext()
     const user=JSON.parse(localStorage.getItem('profile') ||'{}')
-    const userLikedPost=false
+    const userId=user?.result?.googleId||user?.result?._id
+    const userLikedPost=likes?.find(x=>x===userId)
   return (
     <div className='w-[300px] h-[430px] m-4 pb-2 rounded-lg overflow-hidden bg-slate-100 cShadow'>
         <div className='w-full h-[50%] relative'>
@@ -23,7 +25,7 @@ const Post = ({_id,name,createdAt,file,info,likes,tags,title}:postDataType) => {
             <h1 className='text-xl'>{title}</h1>
             <p className='text-gray-600 my-2'>{info.slice(0,60)}...</p>
             <div className='flex w-full text-md items-center justify-between'>
-                <div className='flex cursor-pointer text-blue-400 items-center'>{userLikedPost?<FaRegThumbsUp/>:<FaThumbsUp/>}<span className='ml-1 font-bold'>{likes?.length}</span></div>
+                <div onClick={_=>likePost(_id,false)} className='flex cursor-pointer text-blue-400 items-center'>{!userLikedPost?<FaRegThumbsUp/>:<FaThumbsUp/>}<span className='ml-1 font-bold'>{likes?.length}</span></div>
                 <Link to={`/posts/${_id}`} className="w-fit cBtn">Details</Link>
             </div>
         </div>
--- client/src/pages/DetailsPage.tsx
+++ client/src/pages/DetailsPage.tsx
@@ -1,16 +1,35 @@
 import {useEffect,useState} from 'react'
+import { FaRegThumbsUp, FaThumbsUp, FaTrash } from 'react-icons/fa'
 import { useParams, useNavigate, Link} from 'react-router-dom'
 import { useGlobalContext } from '../context'
 
 const DetailsPage = () => {
     const {id}=useParams()
-    const {getPost,state}=useGlobalContext()
+    const {getPost,state,likePost}=useGlobalContext()
+    const user=JSON.parse(localStorage.getItem('profile') || '{}') 
+    const userId=user?.result?.googleId||user?.result?._id
+    const userLikedPost=state.post?.likes?.find(x=>x===userId)    
 
     useEffect(()=>{
         getPost(id)
     },[id])
   return (
-    <div>{state.post?.title}</div>
+    <div className='w-[95%] md:w-[80%] mx-auto'>
+        <div className='w-full rounded my-4 cShadow bg-slate-100 p-4 flex justify-between flex-col md:flex-row'>
+            <img className='w-full md:w-[40%] rounde-lg object-cover' src={state.post?.file}/>
+            <div className='w-full md:w-[50%]'>
+                <h1 className='text-4xl'>{state.post?.title}</h1>
+                <div className='flex items center my-2 text-gray-500'>{state.post?.tags?.map((tag,i)=><Link to={`/tags/${tag}`} className='mx-[1px] hover:text-gray-700' key={i}><span className='text-sm'>#</span>{tag}</Link>)}</div>
+                <p className='text-lg my-2'>{state.post?.info}</p>
+                <p className='text-lg italic text-gray-800 flex items-center'>Created by: <Link to={`/creator/${state.post?.name}`} className='hover:text-gray-600'>{state.post?.name}</Link></p>
+                <div className='flex w-[95%] text-lg my-2 items-center justify-between'>
+                    <div onClick={_=>likePost(id,true)} className='flex cursor-pointer text-blue-400 items-center'>{!userLikedPost?<FaRegThumbsUp/>:<FaThumbsUp/>}<span className='ml-1 font-bold'>{state.post?.likes?.length}</span></div>
+                    {userId===state.post?.creator && <FaTrash className='text-red-500 cursor-pointer'/>}
+                </div>
+            </div>
+
+        </div>
+    </div>
   )
 }
 
