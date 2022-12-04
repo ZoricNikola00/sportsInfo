b1bfbdb6c7f6efdad062900ea97575eb7a83149d
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 18:08:50 +0100

    Add loader

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { postDataType, useGlobalContext } from '../context'
 import Post from './Post'
+import { DotLoader } from 'react-spinners'
 
 const Posts = () => {
     const {state}=useGlobalContext()
 
+    if(state.loading){
+      return <DotLoader color='rgb(59 130 246)' className=' w-full mx-auto my-24'>LOADING!!!!</DotLoader>
+     }
   return (
     <div className='flex flex-wrap my-4 justify-evenly w-full'>
         {state.posts?.map((post:postDataType)=>{
