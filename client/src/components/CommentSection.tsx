2fd16aef4c246a02621dd74fca63478ab9689307
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 23:55:27 +0100

    Create and edit comment section

--- client/src/components/CommentSection.tsx
+++ client/src/components/CommentSection.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import {useState} from 'react'
 type CommentType={
     id:string|undefined,
     comments:string[] | undefined
 }
 const CommentSection = ({id,comments}:CommentType) => {
+    const [comment,setComment]=useState('')
+    const user=JSON.parse(localStorage.getItem('profile') || '{}') 
+
+    const handleSubmit=(e:React.FormEvent)=>{
+        e.preventDefault()
+
+    }
   return (
-    <div>CommentSection</div>
+    <div className='w-full rounded my-4 flex justify-between cShadow bg-slate-100 p-4'>
+        <div className='w-[59%] h-[300px] p-2 overflow-y-scroll'>
+            <h1 className='text-lg text-gray-600 '>Comments: </h1>
+            <div className='p-3 cSHadow bg-white rounded '>
+              {comments?.map((x,i)=>{
+                const comment=x.split(':')
+                return <div key={i}><span className='text-gray-800 italic'>{comment[0]}</span> : <span className='text-lg'>{comment[1]}</span></div>
+              })}
+            </div>
+        </div>
+        <form onSubmit={handleSubmit} className='w-[39%]'>
+            <textarea value={comment} onChange={(e)=>setComment(e.target.value)} className='cInput h-[60%]' placeholder='Leave comment here...'/>
+            <button type='submit' className='cBtn'>Submit Comment</button>
+        </form>
+    </div>
   )
 }
 
