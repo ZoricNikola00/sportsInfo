fa359469db84e7f11585ea27694ca3a6593bbc4b
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 01:50:15 +0100

    Add auth middleware

--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -1,9 +1,9 @@
 import express from 'express'
 import {addPost, getPosts} from '../controller/post.js'
-
+import auth from '../middleware/auth.js'
 const router=express.Router()
 
 router.get('/',getPosts);
-router.post('/', addPost)
+router.post('/',auth, addPost)
 
 export default router
