dca52c6938b0a35e2b1a64c0eabedb1d931ea077
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 18:10:17 +0100

    Create axios request for search creator

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -13,6 +13,7 @@ API.interceptors.request.use((req:any)=>{
 export const getPosts=(currentPage:number|string)=>API.get(`/posts?page=${currentPage}`)
 export const addPost=(data:postDataType)=>API.post('/posts',data)
 export const searchPosts=(searchData:SearchData)=>API.get(`/posts/search?searchQuery=${searchData.term || 'none'}&tags=${searchData.tags}`)
+export const searchCreator=(creator:string|undefined)=>API.get(`/posts/creator?name=${creator}`)
 
 export const signIn=(data:userData)=>API.post('/user/signin', data)
 export const signUp=(data:userData)=>API.post('/user/signup', data)
