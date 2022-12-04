17f2245c6022fad029d348ab1e818ea950f46ea0
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 18:00:39 +0100

    Create route request for search

--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -1,9 +1,9 @@
 import express from 'express'
-import {addPost, getPosts} from '../controller/post.js'
+import {addPost, getPosts, searchPost} from '../controller/post.js'
 import auth from '../middleware/auth.js'
 const router=express.Router()
 
 router.get('/',getPosts);
 router.post('/',auth, addPost)
-
+router.get('/search', searchPost)
 export default router
