170abb50a2ea148e99cf1010b24dccd1586378fc
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 18:16:16 +0100

    Create request for search creator

--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import {addPost, getPosts, searchPost} from '../controller/post.js'
+import {addPost, getPosts, searchCreator, searchPost} from '../controller/post.js'
 import auth from '../middleware/auth.js'
 const router=express.Router()
 
 router.get('/',getPosts);
 router.post('/',auth, addPost)
 router.get('/search', searchPost)
+router.get('/creator', searchCreator)
 export default router
