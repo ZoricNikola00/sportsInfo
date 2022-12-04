89fd317825b7b0d7df0b93cd0e2252377c1d4bf3
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 14:45:12 +0100

    Wrap provider with router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,10 +7,10 @@ import { AppProvider } from './context'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppProvider>
-      <Router>
+    <Router>
+      <AppProvider>
         <App/>
-      </Router>
-    </AppProvider>
+      </AppProvider>
+    </Router>
   </React.StrictMode>
 )
