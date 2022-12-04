5cb4e6ae44ba7295a02cc3977ebf6c33d93f8747
ZoricNikola00 <zoriclfc@gmail.com>
2022-12-04 01:51:52 +0100

    Add interceptors for auth token

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -3,6 +3,12 @@ import { postDataType, userData } from "../context";
 
 const API=axios.create({baseURL:'http://localhost:5000/'})
 
+API.interceptors.request.use((req:any)=>{
+    if(localStorage.getItem('profile')){
+        req.headers.authorization=`Bearer ${JSON.parse(localStorage.getItem('profile') || '{}').token}`
+    }
+    return req
+})
 
 export const getPosts=(currentPage:number|string)=>API.get(`/posts?page=${currentPage}`)
 export const addPost=(data:postDataType)=>API.post('/posts',data)
